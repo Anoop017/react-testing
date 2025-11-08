81f1c46ebdf017fe35b1f81abec6712de0da7906
Anoop017 <anoopsnair1123@gmail.com>
2025-11-08 18:56:47 +0530

    added counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
+function App(){
+
   const [count, setCount] = useState(0)
 
-  return (
+  const increment = ()=>{
+    setCount(count+1)
+  }
+
+  return(
     <>
-      <h1>Hello!</h1>
-      <p>Fifth testing also failed because i have a practical exam tomorrow</p>
+    
+      <button onClick={increment}>Increase</button>
+      <p>count is : {count}</p>
+    
     </>
   )
 }
 
-export default App
+export default App;
