a56d73696eee9f0028c79809ac7e205ba22fcee2
Anoop017 <anoopsnair1123@gmail.com>
2025-11-12 00:14:09 +0530

    added the first step of to do app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import {useState} from 'react'
 
 function App(){
   const [text, setText] = useState("")
+  const [items, setItems] = useState([])
 
   const insideText = (e) =>{
     setText(e.target.value)
   }
 
+  const addItems = () =>{
+    setItems(oldArray =>[...oldArray, text])
+  }
+
+
   return(
     <>
     <input type="text" value={text} onChange={insideText} placeholder='Enter Your Text Here'/>
+    <button onClick={addItems}>Add Items</button>
     <p>The text is {text}</p>
+    <p>The List are as follows : {items}</p>
     </>
   )
 
