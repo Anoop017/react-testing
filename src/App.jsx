11761c02e7bdfaf1f8fdad79594df7147efd649f
Anoop017 <anoopsnair1123@gmail.com>
2025-11-14 00:22:53 +0530

    updated the to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import {useState} from 'react'
 
 function App(){
-  const [items, setItems] = useState(0)
-  const [text, setText]= useState([])
+  const [text, setText]= useState("")
+  const [items, setItems] = useState([])
+
+
+  const liveText = (e) =>{
+    setText(e.target.value)
+  }
+
+
+
+  const addItem =()=>{
+    setItems(oldList=>[...oldList, text])
+  }
 
 
   return (
     <>
+  <input style={{fontSize:"100px", padding:"15px" ,margin:"15px"}} type="text" placeholder="Enter your Text"
+    value={text} 
+    onChange={liveText}
+  />
+  <br />
+  <button style={{ display:"flex",marginTop:"15px", background:"SkyBlue", borderRadius:"15px" ,fontSize:"75px" ,margin:"auto" }} 
 
+  onClick={addItem}>
+    
+    
+  Add Item</button>
+
+  <ul>
+
+    {items.map((item)=> (
+      <li>{item}</li>
+    ))}
+    
+    </ul>
 
 
     </>
   )
 }
+
+export default App
