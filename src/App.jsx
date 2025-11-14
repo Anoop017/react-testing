ab1cd28de838cc3c39328b822a5162f5e1101bc9
Anoop017 <anoopsnair1123@gmail.com>
2025-11-14 23:37:23 +0530

    studied the react again

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,31 @@
 import {useState} from 'react'
 
 function App(){
-  const [text, setText]= useState("")
-  const [items, setItems] = useState([])
+  const [text, setText] = useState("")
+  const [list, setList] = useState([])
 
+  const addItems = () =>{
+    setList(oldList=>[...oldList,text])
+    
+  }
 
-  const liveText = (e) =>{
+  const liveText = (e)=>{
     setText(e.target.value)
   }
-
-
-
-  const addItem =()=>{
-    setItems(oldList=>[...oldList, text])
-  }
-
-
   return (
     <>
-  <input style={{fontSize:"100px", padding:"15px" ,margin:"15px"}} type="text" placeholder="Enter your Text"
-    value={text} 
-    onChange={liveText}
-  />
-  <br />
-  <button style={{ display:"flex",marginTop:"15px", background:"SkyBlue", borderRadius:"15px" ,fontSize:"75px" ,margin:"auto" }} 
+    <input type="text" value={text} placeholder='Type Something' onChange={liveText} />
+    <br />
+    <button onClick={addItems}>Add Items</button>
 
-  onClick={addItem}>
-    
-    
-  Add Item</button>
 
-  <ul>
-
-    {items.map((item)=> (
-      <li>{item}</li>
-    ))}
-    
+    <ul>
+      {list.map((list)=>(
+        <li>{list}</li>
+      ))}
     </ul>
-
-
     </>
   )
 }
 
-export default App
+export default App;
